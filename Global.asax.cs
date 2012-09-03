1e93459bf1051bcdcfc1b6779a5c9dc3369e4dd3
Daniel Miladinov <daniel.miladinov@gmail.com>
2012-09-02 21:52:56 -0400

    Add updated Global.asax.cs for updated route configuration.

---
+++ Global.asax.cs
@@ -0,0 +1,23 @@
+public class MvcApplication : System.Web.HttpApplication {
+
+    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+        filters.Add(new HandleErrorAttribute());
+    }
+
+    public static void RegisteRoutes(RouteCollection routes) {
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        routes.MapRoute(
+            "Default",
+            "{controller}/{action}/{id}",
+            new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+        );
+    }
+
+    protected void Application_Start() {
+        AreaRegistration.RegisterAllAreas();
+
+        RegisterGlobalFilters(GlobalFilters.Filters);
+        RegisterRoutes(RouteTable.Routes);
+    }
+}
